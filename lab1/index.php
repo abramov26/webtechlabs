e460a08945e91e1873f34fd20d217709fb1e99af
abramov26 <project26@vlad.ac>
2022-11-12 20:19:19 +0500

    Add lab1 files

---
+++ lab1/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Оглавление</title>
+</head>
+<body>
+  <h1>Веб-технологии, Лабораторная работа №1</h1>
+  <h2>Абрамов Владислав, ФИРТ ПИ-323</h2>
+  <a href="/">← Вернуться на главную</a>
+  <ul>
+    <li><a href="/lab1/Lab1.html">Задание № 1</a></li>
+    <li><a href="/lab1/Lab2.html">Задание № 2</a></li>
+    <li><a href="/lab1/Lab3.html">Задание № 3</a></li>
+    <li><a href="/lab1/Lab4.html">Задание № 4</a></li>
+    <li><a href="/lab1/Lab5.html">Задание № 5</a></li>
+    <li><a href="/lab1/Lab6.html">Задание № 6</a></li>
+    <li><a href="/lab1/Lab7.html">Задание № 7</a></li>
+</ul>
+</body>
+</html>
---
+++ lab1/lab2.js
@@ -0,0 +1,4 @@
+function helloMsg() {
+	alert("приветствую!");
+}
+helloMsg();
